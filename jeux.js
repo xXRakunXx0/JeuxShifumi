b8d9ee8b025cc98e76a327e7843ae4481d7b25c0
xXRakunXx0 <alan.dedin42@gmail.com>
2025-05-14 10:17:37 +0200

    DebutDuJeux

---
+++ jeux.js
@@ -0,0 +1,13 @@
+// Choisie ton signe ou ( Ecrire ).
+// Valider ton choix .
+// La Partie commence .
+// Le bot fait son choix .
+// Les deux joueur rélève le choix qui son fait. (De base la phrase ces : Dire au bot le choix qui la fait .)
+// Si le signe du bot est (pierre) et le mien et (feuille) il perd .
+// Si le signe du bot est (ciseaux) et le mien (feuil) il gagne . 
+
+
+function EcrireTonSigne(){
+
+}
+EcrireTonSigne ()
